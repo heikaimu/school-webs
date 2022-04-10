a9b1a5d374731dd212e6f8fcc76a2c3624e0e528
YaowenLiu <576079353@qq.com>
2022-04-10 22:48:27 +0800

    feat: 切片

--- webs/schools/yzb-web/js/theme.js
+++ webs/schools/yzb-web/js/theme.js
@@ -10,11 +10,67 @@
     resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
     recalc = function () {
       var clientWidth = window.innerWidth || docEl.clientWidth;
-      $("html").css({ 'font-size': 16 * (clientWidth / 1920) >= 6 ? 16 * (clientWidth / 1920) : 6 + 'px' })
+      $('html').css({ 'font-size': 16 * (clientWidth / 1920) >= 6 ? 16 * (clientWidth / 1920) : 6 + 'px' });
     };
   recalc();
   win.addEventListener(resizeEvt, recalc, false);
   doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
 
+function footerScroll() {
+  if ($(document).height() - $(document).scrollTop() <= window.innerHeight + window.innerHeight / 2) {
+    let scroll_bottom = $(document).height() - $(document).scrollTop() - window.innerHeight;
+    let half_height = window.innerHeight / 2;
+    $('.footer-content').css({
+      opacity: Math.floor(((half_height - scroll_bottom) / half_height) * 100) / 100,
+      transform: `translateY(${scroll_bottom / 10}px)`,
+    });
+  }
+}
 
+// 导航
+!(function () {
+  $('.web-header').each(function () {
+    var header = $(this);
+    var search = $(this).find('.search-bar');
+    var searchOpenSwitch = $(this).find('.search-bar-show');
+    var searchCloseSwitch = $(this).find('.search-bar-close');
+    var navSwitch = $(this).find('.web-nav-switch');
+    var nav = $(this).find('.web-nav');
+    var mobileBlank = $(this).find('.mobile-blank');
+
+    nav.lavaLamp({ fx: 'swing', speed: 500 });
+
+    searchOpenSwitch.click(function () {
+      search.slideDown();
+    });
+
+    searchCloseSwitch.click(function () {
+      search.slideUp();
+    });
+
+    header.mouseleave(function () {
+      search.slideUp();
+    });
+
+    navSwitch.click(function () {
+      if (navSwitch.hasClass('active')) {
+        navSwitch.stop().removeClass('active');
+        nav.removeClass('active');
+        $('html').css({ 'overflow-y': 'auto' });
+      } else {
+        navSwitch.addClass('active');
+        mobileBlank.fadeIn();
+        nav.addClass('active');
+        $('html').css({ 'overflow-y': 'hidden' });
+      }
+    });
+
+    mobileBlank.click(function () {
+      mobileBlank.fadeOut();
+      navSwitch.stop().removeClass('active');
+      nav.removeClass('active');
+      $('html').css({ 'overflow-y': 'auto' });
+    });
+  });
+})();

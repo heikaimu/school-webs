c60e2e65196fba0dd5d892e8b29d84fb2fda53e5
Liu YaoWen <576079353@qq.com>
2022-04-21 13:11:35 +0800

    chore: 项目修改

---
+++ webs/2022-4-zby-xiamen/js/theme.js
@@ -0,0 +1,124 @@
+/*
+ * @Date: 2022-04-08 17:12:42
+ * @LastEditors: Yaowen Liu
+ * @LastEditTime: 2022-04-19 16:09:11
+ * @FilePath: /school-webs/webs/school-1/js/theme.js
+ */
+
+// footer滚动效果
+function footerScroll() {
+  if ($(document).height() - $(document).scrollTop() <= window.innerHeight + window.innerHeight / 2) {
+    let scrollBottom = $(document).height() - $(document).scrollTop() - window.innerHeight;
+    let halfHeight = window.innerHeight / 2;
+    $('.footer-content').css({
+      opacity: Math.floor(((halfHeight - scrollBottom) / halfHeight) * 100) / 100,
+      transform: `translateY(${scrollBottom / 10}px)`,
+    });
+  }
+}
+
+// 移动端导航
+function headerToggle() {
+  $('.web-header').each(function () {
+    var navSwitch = $(this).find('.web-nav-switch');
+    var nav = $(this).find('.web-nav');
+    var mobileBlank = $(this).find('.mobile-blank');
+
+    nav.lavaLamp({ fx: 'swing', speed: 500 });
+
+    navSwitch.click(function () {
+      if (navSwitch.hasClass('active')) {
+        navSwitch.stop().removeClass('active');
+        nav.removeClass('active');
+        $('html').css({ 'overflow-y': 'auto' });
+      } else {
+        navSwitch.addClass('active');
+        mobileBlank.fadeIn();
+        nav.addClass('active');
+        $('html').css({ 'overflow-y': 'hidden' });
+      }
+    });
+
+    mobileBlank.click(function () {
+      mobileBlank.fadeOut();
+      navSwitch.stop().removeClass('active');
+      nav.removeClass('active');
+      $('html').css({ 'overflow-y': 'auto' });
+    });
+  });
+}
+
+// 导航固定
+function headerFixed() {
+  const scrollTop = $(document).scrollTop();
+  const tipHeight = $('.tip-img').height();
+  const headerHeight = $('.web-header').height();
+  if (scrollTop >= tipHeight) {
+    $('.web-header').css({ position: 'fixed', zIndex: 99997, top: -tipHeight - 1 + 'px' });
+    $('body').css({ paddingTop: `${headerHeight}px` });
+  } else {
+    $('.web-header').css({ position: 'relative', top: 0 });
+    $('body').css({ paddingTop: `${0}px` });
+  }
+}
+
+// 回到顶部
+function backTop() {
+  $('body,html').animate(
+    {
+      scrollTop: 0,
+    },
+    300,
+  );
+}
+
+// select
+function customSelector() {
+  $('.custom-selector').each(function () {
+    const selector = $(this);
+    const placeholder = $(this).find('.placeholder');
+    const options = $(this).find('.options');
+    placeholder.click(function () {
+      $('.custom-selector .options').hide();
+      options.show();
+    });
+  });
+
+  $('body').bind('click', function (e) {
+    if (['placeholder'].includes(e.target.className)) {
+      return;
+    }
+
+    $('.custom-selector .options').hide();
+  });
+}
+
+function mobileNav() {
+  $('#mobileOpen').click(function () {
+    $('#mobileNav').fadeIn();
+  });
+  $('#mobileClose').click(function () {
+    $('#mobileNav').fadeOut();
+  });
+  $('#mobileWxMenu').click(function () {
+    $('#mobileWxImage').addClass('active');
+  });
+  $('#mobileWxImage').click(function () {
+    $(this).removeClass('active');
+  });
+}
+
+// 导航
+$(function () {
+  headerToggle();
+  backTop();
+  footerScroll();
+  customSelector();
+  mobileNav();
+});
+
+// 窗口滚动
+window.addEventListener('scroll', function () {
+  footerScroll();
+  // headerFixed();
+});
---
+++ webs/2022-4-zby-xiamen/js/toTop.js
@@ -0,0 +1,34 @@
+/*
+ * @Date: 2022-04-15 13:18:00
+ * @LastEditors: Yaowen Liu
+ * @LastEditTime: 2022-04-15 13:19:32
+ * @FilePath: /school-webs/webs/school-1/js/toTop.js
+ */
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+});
